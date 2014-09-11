0bf3d81f6bf7414ee3dc874b5223327dd0ad0ca3
sonicwolfsdutch <sonicwolfsdutch@users.noreply.github.com>
2014-09-11 12:18:34 +0200

    if found ur uuid
    if found out ur uuid if chekt it on 2 sites and they gave the same uuid http://minecraft-techworld.com/uuid-lookup-tool and http://minecraft-skin-viewer.com/player/PogoStick29

--- src/me/pogostick29dev/bloodbath/listeners/PlayerJoin.java
+++ src/me/pogostick29dev/bloodbath/listeners/PlayerJoin.java
@@ -9,7 +9,7 @@ public class PlayerJoin implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		if (e.getPlayer().getName().equals("PogoStick29")) { // Should use my UUID...
+		if (e.getPlayer().getName().equals("c793afb5c4b74fdba100b761315913c4")) { /* http://minecraft-techworld.com/uuid-lookup-tool */
             e.setJoinMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Welcome PogoStick29, developer of Bloodbath!");
         }
 	}
